9b461f0e45e18594552745ee3c0b9cd1f8da7b1c
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2023-12-26 17:52:50 +0900

    Title: 달에서 무게 구하기, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231226/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int h1 = 13;
+        float h2 = 0.165f;
+
+        System.out.printf("%d * %.6f = %.6f", h1,h2,h1*h2);
+    }
+}
