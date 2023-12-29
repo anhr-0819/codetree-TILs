1aedab21eab88fc345c7cc5f7ab1562bc202d0fb
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2023-12-29 09:03:36 +0900

    Title: 데이터 교환, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231229/데이터 교환/exchange-data.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+
+        int temp = b;
+
+        b = a;
+        a = c;
+        c = temp;
+
+        System.out.printf("%d\n%d\n%d",a,b,c);
+
+
+    }
+}
