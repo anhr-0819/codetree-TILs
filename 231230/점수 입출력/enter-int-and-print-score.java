ce722b8f07be6efc9df38c5711d295f0b1e10422
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2023-12-30 09:43:01 +0900

    Title: 점수 입출력, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231230/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.printf("Your score is %d point.",n);
+    }
+}
