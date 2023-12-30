0549dbfc9a2979387527d1264740dd485ef7ecaa
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2023-12-30 09:44:37 +0900

    Title: 입력받아 계산 4, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231230/입력받아 계산 4/input-calculate-4.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = new Scanner(System.in).nextInt();
+        System.out.print(a*2);
+    }
+}
