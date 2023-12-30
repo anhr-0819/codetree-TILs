12b7ec5f0c9cb301519c42fefac10f3452d88a22
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2023-12-30 09:47:33 +0900

    Title: 실수 받아 그대로 출력, Time: 141ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 231230/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double n = new Scanner(System.in).nextDouble();
+        System.out.printf("%.2f",n);
+    }
+}
