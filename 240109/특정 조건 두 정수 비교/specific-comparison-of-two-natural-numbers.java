da9dfb2c3ec81c6ace647ea8cca66690fc48cd18
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-09 22:47:32 +0900

    Title: 특정 조건 두 정수 비교, Time: 177ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240109/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        if(a<b) {
+            System.out.print(1 + " ");
+        } else {
+            System.out.print(0 + " ");
+        }
+        if(a == b) {
+            System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
+
+    }
+}
