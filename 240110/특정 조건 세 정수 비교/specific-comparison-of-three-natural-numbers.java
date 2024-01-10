5b1d830846f8f75a6559b056f43a585aa5a964db
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-10 21:15:29 +0900

    Title: 특정 조건 세 정수 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240110/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
+++ 240110/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -3,21 +3,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
+        int min = a;
         int b = scan.nextInt();
+        min = min<= b ? min : b;
         int c = scan.nextInt();
-        if(a<=b) {
-            if(b<=c) {
-                System.out.print(1 + " ");
-            } else {
-                System.out.print(0 + " ");
-            }
+        min = min<= c ? min : c;
+        
+        if(a === min) {
+            System.out.print(1 + " ");
         } else {
             System.out.print(0 + " ");
         }
+
         if(a == b && b == c) {
             System.out.print(1);
         } else {
             System.out.print(0);
-            }
+        }
     }
 }
