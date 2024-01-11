e1753d6dadb6f7fa6ce344b6cd136d3b9efb1a9c
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-11 17:51:39 +0900

    Title: 범위 밖의 수, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240111/범위 밖의 수/number-out-of-range.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        if(a<10|| a>20) {
+            System.out.print("yes");
+        } else {
+            System.out.print("no");
+        }
+    }
+}
