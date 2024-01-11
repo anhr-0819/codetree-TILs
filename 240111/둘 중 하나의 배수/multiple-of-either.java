368d8498010c8c674df28ba1d71c6dfe5f6f9f9c
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-11 17:55:30 +0900

    Title: 둘 중 하나의 배수, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240111/둘 중 하나의 배수/multiple-of-either.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        if (a%3 == 0 || a%5 == 0 ) {
+            System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
+    }
+}
