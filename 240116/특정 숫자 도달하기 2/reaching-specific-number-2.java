de39b932c209d97c63df7b2e1e8c9ec1d185eb2c
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-16 23:05:21 +0900

    Title: 특정 숫자 도달하기 2, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240116/특정 숫자 도달하기 2/reaching-specific-number-2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        while(1<=n) {
+            System.out.print(n + " ");
+            n--;
+        }
+    }
+}
