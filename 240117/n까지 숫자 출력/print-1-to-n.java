936c94621fab54faa1906951f8fd5df24ec26ae1
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-17 23:32:52 +0900

    Title: n까지 숫자 출력, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240117/n까지 숫자 출력/print-1-to-n.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        int i = 1;
+        while(i<=n) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
