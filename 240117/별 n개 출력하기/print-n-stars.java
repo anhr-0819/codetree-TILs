484eee45ed58f8212a9a2bf92461318b2440f781
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-17 23:35:14 +0900

    Title: 별 n개 출력하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240117/별 n개 출력하기/print-n-stars.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        while(n>0) {
+            System.out.println("*");
+            n--;
+        }
+    }
+}
