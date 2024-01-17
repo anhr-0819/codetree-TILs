5f6d791baa75df3ee96a40a2ec2f51c598e82d52
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-18 00:25:12 +0900

    Title: 짝수만 출력, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240118/짝수만 출력/output-only-even.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        while(a<=b) {
+            System.out.print(a + " ");
+            a += 2;
+        }
+    }
+}
