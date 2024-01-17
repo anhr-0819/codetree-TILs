d186f64c0c42aba3af9dd8e674c74c1b82441964
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-18 00:26:40 +0900

    Title: 감소시키며 출력하기 2, Time: 78ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240118/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 26;
+        while(10<=i) {
+            System.out.print(i + " ");
+            i--;
+        }
+    }
+}
