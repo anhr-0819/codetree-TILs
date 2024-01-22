7bf356f12b317f9a973446ecddc7153693db23f4
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-22 15:11:12 +0900

    Title: 100점 도달하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240122/100점 도달하기/reach-100-point.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        for(n<=100) {}
+
+        if(n>=90) {
+            System.out.print("A");
+        } else if(n>=80) {
+            System.out.print("B");
+        } else if(n>=70) {
+            System.out.print("C");
+        } else if(n>=60) {
+            System.out.print("D");
+        } else {
+            System.out.print("F");
+        }
+    }
+}
