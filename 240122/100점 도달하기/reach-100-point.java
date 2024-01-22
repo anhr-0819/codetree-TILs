fb20d5fa8260d54ed6bcda1bec7e8c9fc56a3bc1
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-22 15:14:06 +0900

    Title: 100점 도달하기, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240122/100점 도달하기/reach-100-point.java
+++ 240122/100점 도달하기/reach-100-point.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int n = new Scanner(System.in).nextInt();
-        for(n<=100) {}
-
-        if(n>=90) {
-            System.out.print("A");
-        } else if(n>=80) {
-            System.out.print("B");
-        } else if(n>=70) {
-            System.out.print("C");
-        } else if(n>=60) {
-            System.out.print("D");
-        } else {
-            System.out.print("F");
+        while(n<=100) {
+            if(n>=90) {
+                System.out.print("A ");
+            } else if(n>=80) {
+                System.out.print("B ");
+            } else if(n>=70) {
+                System.out.print("C ");
+            } else if(n>=60) {
+                System.out.print("D ");
+            } else {
+                System.out.print("F ");
+            }
+            n++;
         }
+
     }
 }
