ae11638f0819d65ffeda1e60584da933e6e0ae0c
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-24 11:51:47 +0900

    Title: 등장횟수 확인, Time: 188ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240124/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int cnt = 0;
+        for(int i = 0; i<5; i++) {
+            int n = scan.nextInt();
+            if(n%2 == 0) cnt++;
+        }
+        System.out.print(cnt);
+    }
+}
