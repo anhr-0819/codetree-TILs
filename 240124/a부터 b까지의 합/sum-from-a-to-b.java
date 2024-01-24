98faf62d38cf398d83f46455760026816628fe94
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-24 13:30:41 +0900

    Title: a부터 b까지의 합, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240124/a부터 b까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        int sum = 0;
+        while(a<=b) {
+            sum += a;
+            a++;
+        }
+
+        System.out.print(sum);
+    }
+}
