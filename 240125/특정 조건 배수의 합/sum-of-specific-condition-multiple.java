0786d65d54c08c7bcd14410c2276183999e689f5
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-25 11:28:44 +0900

    Title: 특정 조건 배수의 합, Time: 3017ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

--- 240125/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
+++ 240125/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
@@ -4,8 +4,11 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
+
+        int max = Math.max(a,b);
+        int min = Math.min(a,b);
         int sum = 0;
-        while(a<=b) {
+        while(min<=max) {
             if(a%5 == 0) {
                 sum+=a;
             }
