56fe0e5e965ac01911888270276fa88867df1ddc
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-26 17:56:42 +0900

    Title: 출석 부르기 2, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

--- 240126/출석 부르기 2/calling-attendance-2.java
+++ 240126/출석 부르기 2/calling-attendance-2.java
@@ -1,28 +1,28 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        try(Scanner scan = new Scanner(System.in)) {
-            for(int i = 0; i<100; i++) {
-                int n = scan.nextInt();
-                switch(n) {
-                    case 1: 
-                        System.out.println("John");
-                        continue;
-                    case 2: 
-                        System.out.println("Tom");
-                        continue;
-                    case 3: 
-                        System.out.println("Paul");
-                        continue;
-                    case 4: 
-                        System.out.println("Sam");
-                        continue;
-                    default:
-                        System.out.println("Vacancy");
-                        break;
-                }
+        Scanner scan = new Scanner(System.in);
+        for(int i = 0; ; i++) {
+            int n = scan.nextInt();
 
-            } 
-        } catch(Exception e) {}
+            switch(n) {
+                case 1: 
+                    System.out.println("John");
+                    continue;
+                case 2: 
+                    System.out.println("Tom");
+                    continue;
+                case 3: 
+                    System.out.println("Paul");
+                    continue;
+                case 4: 
+                    System.out.println("Sam");
+                    continue;
+                default:
+                    System.out.println("Vacancy");
+                    return;
+            }
+
+        }
     }
 }
