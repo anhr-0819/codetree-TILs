d0a3ea9e1a781e97120b6702d8a2a37cda4efb52
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-26 12:40:14 +0900

    Title: 친근하지 않은 수, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240126/친근하지 않은 수/unfriendly-number.java
+++ 240126/친근하지 않은 수/unfriendly-number.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int n = new Scanner(System.in).nextInt();
-        int cnt = 0;
-        while(n-->=1) {
+        int cnt = n;
+        while(n>=1) {
             if(n%2 == 0 || n%3 == 0 || n%5 == 0) {
-                continue;
+                cnt--;
             } 
-            cnt++;
+            n--;
         }
         System.out.print(cnt);
     }
