02a29c4677e16ba372387b67b567de662f6ce9d4
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-27 11:49:51 +0900

    Title: 3n + 1 수열, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240127/3n + 1 수열/3n-plus-1-sequence.java
+++ 240127/3n + 1 수열/3n-plus-1-sequence.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int n = new Scanner(System.in).nextInt();
-        int cnt = 0;
-        for(int i = 1; ;i++) {
+        for(int i = 0; ;i++) {
+            if(n == 1) {
+                System.out.print(i);
+                break;
+            }
             if(n%2 == 0) {
                 n = n/2;
             } else {
                 n = n*3+1;
             }
 
-            if(n == 1) {
-                System.out.print(i);
-                break;
-            }
         } 
     }
 }
