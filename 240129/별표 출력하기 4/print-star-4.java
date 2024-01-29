b977a0a3efeef7a3717211f4bf91c6ee71f4ccaa
Hyeryoung An <65947899+anhr-0819@users.noreply.github.com>
2024-01-29 17:21:31 +0900

    Title: 별표 출력하기 4, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240129/별표 출력하기 4/print-star-4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i<n*2; i++) {
+            int k = i>n ? n-(i-n) : i;
+            for(int j = k; j<=n; j++) {
+                sb.append("* ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
